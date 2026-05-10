c55561e8ef427813d08f204d5a07b0ab5a2a81bd
ernapeters0 <144230882+ernapeters0@users.noreply.github.com>
2026-05-10 08:34:34 -0700

    Update entry_point.sh

--- entry_point.sh
+++ entry_point.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-tunnel-node TUNNEL_AUTH_KEY='YtBHKSE3731248Hfr' &
+tunnel-node --TUNNEL_AUTH_KEY='YtBHKSE3731248Hfr' &
 nohup cloudflared tunnel --edge-ip-version auto --no-autoupdate --protocol http2 run --token "eyJhIjoiZjllNjNiZmUwNjZiOTYwN2YyZTExZDliNDRjYTk0ZjYiLCJ0IjoiMGU0NTRhYTAtNDFlNC00ZWYzLTgxNTEtOTMyMWM4Nzc0YTg3IiwicyI6Ik5URm1OR0UyTWpndE5HVmxOaTAwTkRKa0xXSmtZV1V0WlRBMk5qTTFOV1ZpWm1VeSJ9" >/dev/null 2>&1 &
 sleep 5
 curl -sS http://ip-api.com/json --preproxy socks5://127.0.0.1:8086
